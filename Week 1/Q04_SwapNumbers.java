af79715027b70695d20c0657a9308479833750a9
Shivam <shivamsingh990695@gmail.com>
2026-09-25 11:24:58 +0530

    Add Week 1 Java practice solutions

---
+++ Week 1/Q04_SwapNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q04_SwapNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
